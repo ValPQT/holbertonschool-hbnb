584b1ea12899a6f21a55c814b7b7453fdfa71615
Valentin PASQUIET <valentinpasquiet@gmail.com>
2026-03-13 00:25:02 +0100

    feat : implement SQLALchemy  repository (#31)

--- part3/config.py
+++ part3/config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///hbnb.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-jwt-key')  
 
 config = {
